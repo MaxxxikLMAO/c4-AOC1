6c1243ba70ae6be55777df4fb392f228d65ebf4d
Max Müller <43960767+MaxxxikLMAO@users.noreply.github.com>
2022-01-27 16:09:46 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package AOC1;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // String input =
+
+        final File file = new File("txting.txt");
+        final Scanner sc = new Scanner(file);
+        final HashSet<Coordinate> set = new HashSet<>();
+
+        int x = 0, y = 0;
+
+        final String input = sc.nextLine();
+        final String[] splitter = input.split("");
+
+        for (String s : splitter) {
+            switch (s) {
+                case "<" -> x -= 1;
+                case ">" -> x += 1;
+                case "v" -> y -= 1;
+                case "^" -> y += 1;
+            }
+            final Coordinate coord = new Coordinate(x, y);
+            set.add(coord);
+        }
+        System.out.println(set.size()+1);
+    }
+}
